5133cec590d91449380889a34b341c83ed52720c
sameermd16 <sameerthemc@gmail.com>
2023-06-03 15:50:27 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import react from 'react'
+import reactLogo from '../assets/react.svg'
+
+export default function Header() {
+    return (
+        <nav>
+            <img src='../assets/react.svg' />
+            <h3>ReactFacts</h3>
+            <h4 className="mr-auto">React Course - Project 1</h4>
+        </nav>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+import react from 'react';
+import reactDOM from 'react-dom';
+
+
+export default function Main(){
+    return (
+        <main>
+            <h1>Fun Facts with React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+            </ul>
+        </main>
+    )
+}
